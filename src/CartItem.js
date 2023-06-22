7417563c031b0bc2d1df2847fc447f30481a787b
gupta-sujal <sujalgupta484@gmail.com>
2023-06-22 16:51:40 +0530

    added functionality to change the qty of item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,22 +1,59 @@
 import React from "react";
-export default function CartItem()
-{
-    const styles={
-        image:{
-            height:100,width:200,borderRadius:10,backgroundColor:'skyblue'
-        }
-    }
+// export default function CartItem()
+// {
+//     
+    //     
+    // }
+    class CartItem extends React.Component
+    {
+        constructor(){
+                    super();
+                    this.state={
+                        price:999,
+                        title:'mobile phone',
+                        qty:1,
+                        img:''
+                    }
+                }
+                increaseQuantity=()=>
+                {
+                    this.setState((prevState)=>
+                        {
+                            return{
+                            qty:prevState.qty+1
+                            }
+                        }
+                    )
+                    
+                }
+                decreaseQuantity=()=>
+                {
+                    this.setState((prevState)=>
+                    {
+                        return{
+                            qty:prevState.qty-1
+                            }
+                    }
+                )
+                    
+                }
+
+        render()
+        {
+            
     return(
         <div className="cart-item">
             <div className="left-block">
                 <img style={styles.image}/>
                 </ div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>item</div>
-                    <div style={{fontSize:25}}>price</div>
-                    <div style={{fontSize:25}}>rating</div>
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{fontSize:25}}>{this.state.price}</div>
+                    <div style={{fontSize:25}}>{this.state.qty}</div>
                     <div className="cart-item-actions">
-
+                    <img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"/>
+                    <img alt="decrease" onClick={this.decreaseQuantity} className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828899.png"/>
+                    <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/6861/6861362.png"/>
                     </div>
 
                 </div>
@@ -24,3 +61,10 @@ export default function CartItem()
         </div>
     )
 }
+    }
+    const styles={
+        image:{
+            height:200,width:200,borderRadius:10,backgroundColor:'skyblue'
+        }
+    }
+    export default CartItem;
